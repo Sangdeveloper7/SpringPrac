00c2d129e19ec18107adf1e44676a7c62c5d2a7b
Sangdeveloper7 <leesanghyun9757@gmail.com>
2024-07-01 15:35:28 +0900

    a

---
+++ src/main/java/com/example/springparc/SpringparcApplication.java
@@ -0,0 +1,13 @@
+package com.example.springparc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringparcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringparcApplication.class, args);
+    }
+
+}
